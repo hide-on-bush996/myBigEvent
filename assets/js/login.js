e758f832974fb87d4d62f4c27fe0d1a773ec9318
xiaoyan <xiaoyan@qq.com>
2021-01-03 10:52:22 +0800

    新增index.html文件，更新了login表单功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,10 +14,33 @@ $(function () {
 
     // 从layui内获得form属性
     let form = layui.form;
+    let layer = layui.layer;
     form.verify({
         pwd: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
-        ]
+        ],
+        repwd: function (value) {
+            let pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致！';
+            }
+        }
+    })
+
+    //  注册提交
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        let data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功，请登录！');
+            $('#link_login').click();
+        })
     })
 })
